9fa5ca1dd4a0a777d0a61aa64e657a1d36556363
danie <dsmit2@gmail.com>
2020-05-24 00:35:58 +0200

    initial commitd

--- main.py
+++ main.py
@@ -561,5 +561,5 @@ def my_PracticePost():
     # return render_template('Practice.html')
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=80, threaded=False)
-    app.run()
+    app.run(host='0.0.0.0', port=80, threaded=False)
+    #app.run()
